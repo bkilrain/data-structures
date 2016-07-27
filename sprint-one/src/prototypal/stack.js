1bb116f2e24fd06c26a0ddec8f22dbd26719295f
bkilrain <bkilrain@gmail.com>
2016-07-27 16:40:45 -0700

    Implement prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,49 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.syze = 0;
+  obj.stack = {};
+  obj.storage = {};
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.syze++;
+  this.stack[this.syze] = value;
+};
 
+stackMethods.pop = function() {
+  this.storage[this.syze] = this.stack[this.syze];
+  this.syze--;
+  return this.storage[this.syze + 1];
+};
+
+stackMethods.size = function() {
+  return Math.max(this.syze, 0);
+};
+
+/*
+var Stack = function() {
+  var obj = Object.create(Stack.prototype);
+  obj.syze = 0;
+  obj.stack = {};
+  obj.storage = {};
+  return obj;
+};
+
+var stackMethods = {};
+
+Stack.prototype.push = function(value) {
+  this.syze++;
+  this.stack[this.syze] = value;
+};
+
+Stack.prototype.pop = function() {
+  this.storage[this.syze] = this.stack[this.syze]
+  this.syze--;
+};
+
+Stack.prototype.size = function() {
+  return Math.max(this.syze, 0);
+};*/
