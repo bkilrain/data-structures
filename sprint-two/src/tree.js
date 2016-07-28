39167aa3b449ef26bf69d0a6fcfc4befca5909d8
bkilrain <bkilrain@gmail.com>
2016-07-28 10:46:48 -0700

    Implement functional with shared methods tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  result = false;
+  var search = function(node) {
+    if (node.value === target) {
+      result = true;
+    } else if (node.children.length) {
+      for (var i = 0; i < node.children.length; i++) {
+        search(node.children[i]);
+      } 
+    }
+  };
+  search(this);
+  return result;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+addchild complexity is O(1);
+contains complexity is O(log(n))
+
  */
