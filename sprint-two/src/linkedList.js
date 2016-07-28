61279d8a2adef4a3b96ddd74eadc9f8998f93566
bkilrain <bkilrain@gmail.com>
2016-07-28 09:56:20 -0700

    Implement functional linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,37 @@
 var LinkedList = function() {
   var list = {};
+  var counter = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while (node.value !== target && node !== list.tail) {
+      node = node.next;
+    }
+    if (node.value === target) {
+      return true;
+    } else {
+      return false;
+    }
+
   };
 
   return list;
@@ -26,4 +48,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+addToTail complexity is O(1);
+removeHead complexity is O(1);
+contains complexity is O(n);
+
  */
