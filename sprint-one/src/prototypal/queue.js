93f4713624bde07c92d761dcaabe434d4f5dffc1
bkilrain <bkilrain@gmail.com>
2016-07-27 19:30:11 -0700

    Implement prototypal queue and clean up stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.queue = {};
+  obj.storage = {};
+  obj.syze = 0;
+  obj.keyIn = 0;
+  obj.keyOut = 0;
+  return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.queue[this.keyIn] = value;
+  this.keyIn++;
+  this.syze++;
+};
 
+queueMethods.dequeue = function() {
+  var item = this.queue[this.keyOut];
+  this.storage[this.keyOut] = item;
+  delete this.queue[this.keyOut];
+  this.syze--;
+  this.keyOut++;
+  return item;
+};
+
+queueMethods.size = function() {
+  return Math.max(this.syze, 0);
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -23,28 +23,3 @@ stackMethods.pop = function() {
 stackMethods.size = function() {
   return Math.max(this.syze, 0);
 };
-
-/*
-var Stack = function() {
-  var obj = Object.create(Stack.prototype);
-  obj.syze = 0;
-  obj.stack = {};
-  obj.storage = {};
-  return obj;
-};
-
-var stackMethods = {};
-
-Stack.prototype.push = function(value) {
-  this.syze++;
-  this.stack[this.syze] = value;
-};
-
-Stack.prototype.pop = function() {
-  this.storage[this.syze] = this.stack[this.syze]
-  this.syze--;
-};
-
-Stack.prototype.size = function() {
-  return Math.max(this.syze, 0);
-};*/
