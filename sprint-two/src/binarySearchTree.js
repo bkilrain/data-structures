5657abf3823ca098c3c69d3175dbd2f43c75be5c
bkilrain <bkilrain@gmail.com>
2016-07-28 21:29:04 -0700

    Implement functional with shared methods binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,60 @@
 var BinarySearchTree = function(value) {
+  var bst = {};
+  bst.value = value;
+  bst.left = {};
+  bst.right = {};
+
+  _.extend(bst, BinarySearchTreeMethods);
+  return bst;  
 };
 
+var BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(value) {
+  var traverse = function(node) {
+    if (Object.keys(node.left).length > 0 && node.value > value) {
+      traverse(node.left);
+    } else if (Object.keys(node.right).length > 0 && node.value < value) {
+      traverse(node.right);
+    } else if (node.value > value) {
+      node.left = BinarySearchTree(value);
+    } else if (node.value < value) {
+      node.right = BinarySearchTree(value);
+    }
+  };
+  traverse(this);
+};
+
+BinarySearchTreeMethods.contains = function(value) {
+  results = false;
+
+  var search = function(node) {
+    if (node.value > value) {
+      search(node.left);
+    } else if (node.value < value) {
+      search(node.right);
+    } else if (node.value === value) {
+      results = true;
+    }
+  };
+  search(this);
+  return results;
+};
+
+BinarySearchTreeMethods.depthFirstLog = function(callback) {
+  var goDeep = function(node) {
+    if (Object.keys(node).length > 0) {
+      callback(node.value);
+      goDeep(node.left);
+      goDeep(node.right);
+    }
+  };
+  goDeep(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: O(n);
+ contains: O(n);
+ depthFirstLog: O(n);
  */
