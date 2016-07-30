6854e6b29d9ffcd8ae0fc04def8b73db76fe9ab6
bkilrain <bkilrain@gmail.com>
2016-07-29 20:08:48 -0700

    Begin implementation of doubly linked lists

---
+++ sprint-two/src/doubly-linked-list.js
@@ -0,0 +1,64 @@
+var DoubleLinkedList = function() {
+  var list = {};
+  var counter = 0;
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var newNode = DLLNode(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
+  };
+
+  list.addToHead = function(value) {
+
+  };
+
+  list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
+  };
+
+  list.removeTail = function() {
+
+  };
+
+  list.contains = function(target) {
+    var node = list.head;
+    while (node.value !== target && node !== list.tail) {
+      node = node.next;
+    }
+    if (node.value === target) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return list;
+};
+
+var DLLNode = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+
+addToTail complexity is O(1);
+removeHead complexity is O(1);
+contains complexity is O(n);
+
+ */
